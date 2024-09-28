54a9458172f151392db645833928e29508b004fc
Ivan Petrushev <ivanatora@gmail.com>
2024-09-28 10:13:30 +0300

    tests: killed

--- tests/test_endings.py
+++ tests/test_endings.py
@@ -66,5 +66,15 @@ def test_ending_4(p):
     assert "Mission: fail" in output 
     assert "unfold worldwide" in output
 
+def test_ending_killed(p):
+    """
+    Go through the whole path, check if the desired end is reached.
+    """
+    send_inputs(p, "1,1,3,1,2,1,1,1")
+    p.expect_exact('?>')
+    output = p.before
+    print("Captured Output:", output)
+    assert "eager to shoot" in output
+
 if __name__ == "__main__":
     pytest.main()
